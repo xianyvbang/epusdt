e9ccae2e1ede706fe1627f74c597b4cd01922864
刘梦龙 <893043230@qq.com>
2026-09-23 15:15:25 +0800

    chore: bump dev version to v0.0.9

--- src/config/config.go
+++ src/config/config.go
@@ -28,7 +28,7 @@ var (
 	TgBotToken         string
 	TgProxy            string
 	TgManage           int64
-	BuildVersion       = "0.0.8-dev"
+	BuildVersion       = "0.0.9-dev"
 	BuildCommit        = "none"
 	BuildDate          = "unknown"
 	configRootPath     string
